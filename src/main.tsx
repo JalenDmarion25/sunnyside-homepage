3741d791baf90e6631b9a37685812c5189b05bed
Jalen Hudson <133799911+JalenDmarion25@users.noreply.github.com>
2024-06-05 18:27:21 -0400

    Update main.tsx (Removed a comma)

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename="/sunnyside-homepage/">
   <React.StrictMode>
     <MainPage/>
-  </React.StrictMode>,
+  </React.StrictMode>
   </BrowserRouter>
 )
